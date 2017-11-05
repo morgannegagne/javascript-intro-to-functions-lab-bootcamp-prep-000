1abcc6d100fdf045fe434fb8224c538cac36dac0
Morganne Gagne <morgannegagne@gmail.com>
2017-11-05 18:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shout(string){
-  return string.toUpperCase()
+  console.log(string.toUpperCase())
 }
 
 function whisper(string){
-  return string.toLowerCase()
+  console.log(string.toLowerCase()) 
 }
 
 function sayHiToGrandma(string){
